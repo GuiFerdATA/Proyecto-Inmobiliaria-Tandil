f789802d81c29622614da6afaf2222f1bbdf551a
Guille <guille.fer79@hotmail.com>
2023-10-24 20:48:41 -0300

    agregamos animaciones con javascript scrollreveal

---
+++ JavaScript/animaciones.js
@@ -0,0 +1,24 @@
+window.sr =ScrollReveal();
+
+
+    sr.reveal('.contenedor_header', {
+        duration: 2500,
+        origin:'top',
+        distance: '100px',
+    });
+    sr.reveal('h1', {
+        duration: 2500,
+        origin:'left',
+        distance: '1000px',
+    });
+    sr.reveal('.contenido_inicio a', {
+        duration: 2500,
+        origin:'right',
+        distance: '500px',
+    });
+    sr.reveal('.form-inline', {
+        duration: 2500,
+        origin:'bottom',
+        distance: '500px',
+    });
+    
