dee572d0bb7bf94ec83e52b4c93756f72fc2fdad
Guille <guille.fer79@hotmail.com>
2023-10-25 02:12:13 -0300

    agregamos nueva seccion, terminamos mañana xD

--- JavaScript/animaciones.js
+++ JavaScript/animaciones.js
@@ -3,25 +3,47 @@ window.sr =ScrollReveal();
         duration: 2500,
         origin:'top',
         distance: '100px',
+        reset: true,
     });
     sr.reveal('h1', {
         duration: 2500,
         origin:'left',
         distance: '1000px',
+        reset: true,
     });
     sr.reveal('.contenido_inicio a', {
         duration: 2500,
         origin:'right',
         distance: '500px',
+        reset: true,
     });
     sr.reveal('.form-inline', {
         duration: 1500,
         origin:'bottom',
         distance: '300px',
+        reset: true,
     });
     sr.reveal('.btn_downTwo', {
         duration: 2500,
         origin:'left',
         distance: '500px',
+        reset: true,
     });
-    
+    sr.reveal('.container_derObjetivos', {
+        duration: 2500,
+        origin:'right',
+        distance: '500px',
+        reset: true,
+    });
+    sr.reveal('.container_izqObjetivos', {
+        duration: 2500,
+        origin:'left',
+        distance: '500px',
+        reset: true,
+    });
+    sr.reveal('.h2', {
+        duration: 2500,
+        origin:'bottom',
+        distance: '250px',
+        reset: true,
+    });
