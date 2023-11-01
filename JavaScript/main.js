afb1a2143388e5cb994e0e6357be48ec387a4fcb
Guille <guille.fer79@hotmail.com>
2023-11-01 01:31:57 -0300

    interactuamos con javascript

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,24 +1,28 @@
-/*carga de pagina */ 
+import propiedades from "./propiedades.js";
 
+
+
+/* variables */
+const $enlacesMenu = document.querySelectorAll('.contenedor_header ul li a')
+const $btnDown = document.querySelector('.contenido_secPrincipal a')
+const $rediNuesObj = document.getElementById('#nuestroObjetivo')
+
+
+
+/*carga de pagina */ 
 window.addEventListener('load', function() {
-  // Espera 2 segundos (2000 milisegundos) antes de ocultar el elemento 'loader'
   setTimeout(function() {
     document.getElementById('loader').style.display = 'none';
   }, 300);
 });
 window.addEventListener('load', function() {
-  // Espera 2 segundos (2000 milisegundos) antes de ocultar el elemento 'loader'
   setTimeout(function() {
     document.getElementById('loader2').style.display = 'none';
   }, 600);
 });
 
-/*HOVERS NAV */
 
-const $enlacesMenu = document.querySelectorAll('.contenedor_header ul li a')
-const $btnDown = document.querySelector('.contenido_secPrincipal a')
-const $rediNuesObj = document.getElementById('#nuestroObjetivo')
-
+/* hover nav */
 $enlacesMenu.forEach((items) => {
     items.addEventListener('mouseenter', () => {
         items.style.backgroundColor = 'rgb(249, 183, 0)'
@@ -28,9 +32,8 @@ $enlacesMenu.forEach((items) => {
     })
 });
 
-/*
-REDIRECCIONAMIENTO NAV
-*/
+/* filtrado de búsquedo */
+
 
 
 
---
+++ JavaScript/propiedades.js
@@ -0,0 +1,58 @@
+const propiedades = [
+    {
+        id: 1,
+        tipo: 'Casa',
+        titulo: 'Casa, con cocina/comedor y 3 habitaciones',
+        habitaciones: 3,
+        baños: 2,
+        patio: true,
+        precio: 160000
+    },
+    {
+        id: 2,
+        tipo: 'Departamento',
+        titulo: 'Departamento, con cocina, comedor y 4 habitaciones',
+        habitaciones: 4,
+        baños: 1,
+        patio: false,
+        precio: 120000
+    },
+    {
+        id: 3,
+        tipo: 'Casa',
+        titulo: 'Casa, con cocina/comedor y 3 habitaciones',
+        habitaciones: 5,
+        baños: 2,
+        patio: true,
+        precio: 160000
+      },
+      {
+        id: 4,
+        tipo: 'Departamento',
+        titulo: 'Departamento, con cocina, comedor y 1habitaciones',
+        habitaciones: 1,
+        baños: 1,
+        patio: false,
+        precio: 120000
+      },
+      {
+        id: 5,
+        tipo: 'Casa',
+        titulo: 'Casa, con cocina/comedor y 3 habitaciones',
+        habitaciones: 3,
+        baños: 2,
+        patio: true,
+        precio: 160000
+      },
+      {
+        id: 6,
+        tipo: 'Departamento',
+        titulo: 'Departamento, con cocina, comedor y 3 habitaciones',
+        habitaciones: 3,
+        baños: 1,
+        patio: false,
+        precio: 120000
+      },
+]
+
+export default propiedades;
