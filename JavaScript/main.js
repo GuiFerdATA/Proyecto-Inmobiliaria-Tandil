5d36c550199ef2e625399cc8e9bfccf84a451a52
Guille <guille.fer79@hotmail.com>
2023-11-03 01:37:09 -0300

    algunos cambios y mejoras en resultados

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -70,7 +70,11 @@ $formBusqueda.addEventListener("submit", (event) => {
                         </div>
                     </header>
                 </div>
-                <body id="body_resultados">
+                <svg class="svg2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#1AA3FF" fill-opacity="1"
+                    d="M0,32L120,32C240,32,480,32,720,53.3C960,75,1200,117,1320,138.7L1440,160L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
+                </path>
+            </svg>
                     <div id="resultadoBusqueda">
                         <div class="card">
                             <img src="${propiedad.img}" alt="${propiedad.titulo}">
@@ -84,7 +88,8 @@ $formBusqueda.addEventListener("submit", (event) => {
                             </div>
                         </div>
                     </div>
-                </body>
+                    
+              
             `).join('');
 
             // Insertar el contenido HTML en la ventana de resultados
@@ -102,7 +107,7 @@ function buscarPropiedades(termino) {
     return propiedades.filter(propiedad => {
         return (
             propiedad.titulo.toLowerCase().includes(termino) ||
-            propiedad.tipo.toLowerCase().includes(termino)   
+            propiedad.tipo.toLowerCase().includes(termino)
         );
     });
 }
